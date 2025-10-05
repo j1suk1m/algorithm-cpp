62b13cfe6cc4faff822a933eb0852d3dbd8620fa
Jisu Kim <jisukim.tech@gmail.com>
2025-10-05 16:40:03 +0900

    [level 2] Title: 더 맵게, Time: 233.42 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq; // 최소 힙
+    
+    for (int i = 0; i < scoville.size(); i++) {
+        pq.push(scoville[i]);
+    }
+    
+    while(pq.size() >= 2 && pq.top() < K) {
+        int first = pq.top(); pq.pop();
+        int second = pq.top(); pq.pop();   
+        pq.push(first + second * 2);
+        answer++;
+    }
+    
+    return pq.top() >= K ? answer : -1;
+}
