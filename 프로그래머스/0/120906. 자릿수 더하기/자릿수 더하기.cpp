f9c930b7853755d63e73ed3455a06183d036a92f
Jisu Kim <jisukim.tech@gmail.com>
2025-10-21 00:02:35 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,14 +1,13 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    string number = to_string(n);
     
-    for (int i = 0; i < number.size(); i++) {
-        answer += number[i] - '0';
+    while (n > 0) {
+        answer += n % 10;
+        n /= 10;
     }
     
     return answer;
