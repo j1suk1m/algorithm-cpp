7422117884e281d152446fbdf4bd60db9df26c12
jisukimtech <jisukim.tech@gmail.com>
2025-12-30 22:41:01 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,67 +1,81 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream> // cin, cout
+#include <vector> // vector
+#include <numeric> // accumulate
+#include <algorithm> // sort
 
 using namespace std;
 
-const int ALPHABET_COUNT = 26;
-
 int L, C;
-vector<char> temp; // 현재 조합 시도 중인 암호
-vector<string> answer; // 최종 선택된 암호 목록
+vector<string> alphabets;
+vector<string> selectedAlphabets;
 
-// ch가 모음인지 확인
-bool isVowel(char ch) {
-    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-}
+const int VOWELS_MIN_COUNT = 1;
+const int CONSONANTS_MIN_COUNT = 2;
 
-// temp가 유효한 암호인지 확인
-bool isValid(const vector<char>& temp) {
-    int vowelCount = 0;
-    
-    for (char ch : temp) {
-        if (isVowel(ch)) {
-            vowelCount++;
-        }
-    }
-
-    // 모음이 한 개 이상, 자음이 두 개 이상인지 확인
-    return vowelCount >= 1 && L - vowelCount >= 2;
-}
-
-// 백트래킹으로 알파벳 조합 계산
-void backtrack(int startIndex, vector<char>& temp, const vector<char>& candidates) {
-    if ((int)temp.size() == L) {
-        if (isValid(temp)) {
-            answer.emplace_back(temp.begin(), temp.end());       
-        }
-        
-        return;
-    }
-
-    for (int i = startIndex; i < (int)candidates.size(); i++) {
-        temp.push_back(candidates[i]);
-        backtrack(i + 1, temp, candidates);
-        temp.pop_back();
-    }
-}
+// 프로토타입
+bool isVowel(char);
+bool isValidPassword(string);
+void generatePassword(int, int);
 
 int main() {
     cin >> L >> C;
 
-    vector<char> candidates(C);
+    string alphabet;
 
+    // 알파벳 저장
     for (int i = 0; i < C; i++) {
-        cin >> candidates[i];
+        cin >> alphabet;
+        alphabets.push_back(alphabet);
     }
 
-    sort(candidates.begin(), candidates.end());
-    
-    backtrack(0, temp, candidates);
+    // 알파벳 사전순 정렬
+    sort(alphabets.begin(), alphabets.end());
 
-    for (string s : answer) {
-        cout << s << "\n";
-    }
-    
+    // 유효한 암호 생성
+    generatePassword(0, 0);
+
     return 0;
+}
+
+bool isVowel(char ch) {
+    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+}
+
+bool isValidPassword(string password) {
+    int vowelsCount = 0;
+    int consonantsCount = 0;
+
+    for (char ch : password) {
+        if (isVowel(ch)) {
+            vowelsCount++;
+        } else {
+            consonantsCount++;
+        }
+    }
+
+    return vowelsCount >= VOWELS_MIN_COUNT && consonantsCount >= CONSONANTS_MIN_COUNT;
+}
+
+void generatePassword(int start, int selectedCount) {
+    if (selectedCount == L) {
+        // vector<string> -> string 변환
+        string password = accumulate(
+            selectedAlphabets.begin(),
+            selectedAlphabets.end(),
+            string{}
+        );
+
+        // 유효한 암호일 경우 출력
+        if (isValidPassword(password)) {
+            cout << password << "\n";
+        }
+
+        return;
+    }
+
+    for (int i = start; i < C; i++) {
+        selectedAlphabets.push_back(alphabets[i]);
+        generatePassword(i + 1, selectedCount + 1);
+        selectedAlphabets.pop_back();
+    }
 }
