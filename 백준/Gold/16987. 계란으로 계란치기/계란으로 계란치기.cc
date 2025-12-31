e4949249caf525c29441f3a0185a487c10f5280e
jisukimtech <jisukim.tech@gmail.com>
2025-12-31 09:59:28 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -1,34 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream> // cin, cout
+#include <vector> // vector
+#include <algorithm> // max
 
 using namespace std;
 
+// 구조체
 struct Egg {
-    int durability; // 내구도
-    int weight; // 무게
-    bool isBroken; // 깨짐 여부
+    int strength;
+    int weight;
+    bool isBroken;
 
-    Egg(int durability, int weight) {
-        this->durability = durability;
+    Egg(int strength, int weight) {
+        this->strength = strength;
         this->weight = weight;
         this->isBroken = false;
     }
 
-    // other로 this를 치는 함수
-    void beHitBy(Egg& other) {
-        this->durability -= other.weight;
-        updateBrokenState();
-    }
-
-    // other로 this를 친 상태를 원복하는 함수
-    void recover(Egg& other) {
-        this->durability += other.weight;
-        updateBrokenState();
-    }
-
-    void updateBrokenState() {
-        if (this->durability <= 0) {
+    void updateBrokenStatus() {
+        if (this->strength <= 0) {
             this->isBroken = true;
         } else {
             this->isBroken = false;
@@ -36,71 +25,98 @@ struct Egg {
     }
 };
 
+// 프로토타입
+void hit(Egg&, Egg&);
+void recover(Egg&, Egg&);
+void hitEggs(int, int);
+
 int N;
-int answer = 0;
+int answer;
 vector<Egg> eggs;
-
-void hitEggs(int currEggIndex, int brokenEggCount) {    
-    if (currEggIndex == N) { // 맨 오른쪽 계란에 도달
-        answer = max(answer, brokenEggCount);
-        return;
-    }
-
-    if (eggs[currEggIndex].isBroken) { // 현재 계란이 깨진 경우
-        hitEggs(currEggIndex + 1, brokenEggCount);
-        return;
-    }
-
-    int count = 0; // 현재 계란으로 친 계란 횟수
-    
-    for (int i = 0; i < N; i++) {
-        if (i == currEggIndex) continue;
-
-        Egg& curr = eggs[currEggIndex];
-        Egg& next = eggs[i];
-        
-        if (!next.isBroken) {
-            count++;
-            
-            bool wasCurrBroken = curr.isBroken;
-            bool wasNextBroken = next.isBroken;
-            
-            curr.beHitBy(next);
-            next.beHitBy(curr);
-
-            int newlyBrokenEggCount = 0;
-
-            if (!wasCurrBroken && curr.isBroken) newlyBrokenEggCount++;
-            if (!wasNextBroken && next.isBroken) newlyBrokenEggCount++;
-            
-            hitEggs(currEggIndex + 1, brokenEggCount + newlyBrokenEggCount);
-
-            curr.recover(next);
-            next.recover(curr);
-        }
-    }
-
-    if (count == 0) { // 현재 계란으로 아무 계란도 치지 못한 경우
-        hitEggs(currEggIndex + 1, brokenEggCount);        
-    }
-}
 
 int main() {
     cin >> N;
 
     int S, W;
 
-    // 계란 정보 저장
+    // 계란 저장
     for (int i = 0; i < N; i++) {
         cin >> S >> W;
         eggs.push_back(Egg(S, W));
     }
 
-    if (N > 1) {
-        hitEggs(0, 0);
-    }
+    int start = 0;
+    int totalBrokenCount = 0;
+
+    // 계란 치기
+    hitEggs(start, totalBrokenCount);
 
     cout << answer;
 
     return 0;
+}
+
+void hit(Egg& curr, Egg& next) {
+    curr.strength -= next.weight;
+    next.strength -= curr.weight;
+
+    curr.updateBrokenStatus();
+    next.updateBrokenStatus();
+}
+
+void recover(Egg& curr, Egg& next) {
+    curr.strength += next.weight;
+    next.strength += curr.weight;
+
+    curr.updateBrokenStatus();
+    next.updateBrokenStatus();
+}
+
+void hitEggs(int currIndex, int totalBrokenCount) {
+    if (currIndex == N) {
+        answer = max(answer, totalBrokenCount);
+        return;
+    }
+
+    Egg& curr = eggs[currIndex];
+
+    if (curr.isBroken) {
+        hitEggs(currIndex + 1, totalBrokenCount);
+        return;
+    }
+
+    int hitCount = 0;
+
+    // 손에 들고 있는 계란으로 다른 계란 치기
+    for (int nextIndex = 0; nextIndex < N; nextIndex++) {
+        if (nextIndex == currIndex) continue;
+
+        Egg& next = eggs[nextIndex];
+
+        if (next.isBroken) continue;
+
+        // 새로 깨진 계란 개수를 집계하기 위해 계란 치기 전 상태 저장
+        bool wasCurrBroken = curr.isBroken;
+        bool wasNextBroken = next.isBroken;
+
+        // 계란 치기
+        hit(curr, next);
+        hitCount++;
+
+        int brokenCount = 0;
+
+        // 새로 깨진 계란 개수 집계
+        if (!wasCurrBroken && curr.isBroken) brokenCount++;
+        if (!wasNextBroken && next.isBroken) brokenCount++;
+
+        hitEggs(currIndex + 1, totalBrokenCount + brokenCount);
+
+        // 계란 상태 원복
+        recover(curr, next);
+    }
+
+    // 손에 들고 있는 계란으로 어떤 계란도 치지 못한 경우 다음 계란으로 이동
+    if (hitCount == 0) {
+        hitEggs(currIndex + 1, totalBrokenCount);
+    }
 }
