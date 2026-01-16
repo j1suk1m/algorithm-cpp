82ad86eb9bde8d36f2bdcd7a03ea534eb55477cf
jisukimtech <jisukim.tech@gmail.com>
2026-01-16 10:25:36 +0900

    [Gold II] Title: 면접보는 승범이네, Time: 656 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
+++ 백준/Gold/17835. 면접보는 승범이네/면접보는 승범이네.cc
@@ -2,93 +2,101 @@
 
 using namespace std;
 
+// 인접 리스트에서 사용할 구조체
 struct Edge {
     int dest;
-    long long distance;
+    int cost;
 
-    Edge(int dest, long long distance) {
+    Edge(int dest, int cost) {
         this->dest = dest;
-        this->distance = distance;
-    }
-
-    bool operator<(const Edge& other) const {
-        return distance > other.distance;
+        this->cost = cost;
     }
 };
 
-const long long INF = (long long)1e5 * 1e5 * 5;
+// 다익스트라에서 사용할 구조체
+struct State {
+    int node;
+    long long dist; // 누적 거리 -> int 오버플로 방지
+
+    State(int node, long long dist) {
+        this->node = node;
+        this->dist = dist;
+    }
+
+    bool operator<(const State& other) const {
+        return dist > other.dist;
+    }
+};
+
+const int MAX_N = (int)1e5;
+const int MAX_M = (int)1e5 * 5;
+const int MAX_K = MAX_N;
+const int MAX_C = (int)1e5;
+const long long INF = (long long)MAX_M * MAX_C + 1;
 
 int N, M, K;
-vector<int> multiSources; // 면접장이 배치된 도시 번호
-vector<vector<Edge>> edges; // 인접 리스트
-vector<long long> minDistances; // 도시별 가장 가까운 면접장까지의 최단 거리
+vector<vector<Edge>> edges(MAX_N + 1, vector<Edge>()); // 인접 리스트
+vector<int> interviewSites(MAX_K); // 면접장이 배치된 도시 번호
+vector<long long> dist(MAX_N + 1, INF); // 도시별 가장 가까운 면접장까지의 최단 거리
 
-// 프로토타입
-void findMinDistances();
+void dijkstra(const vector<int>& sources);
 
 int main() {
     cin >> N >> M >> K;
 
-    edges.assign(N + 1, vector<Edge>());
-
     // 인접 리스트 저장
     for (int i = 0; i < M; i++) {
-        int src, dest;
-        long long distance;
-        cin >> src >> dest >> distance;
+        int from, to, cost;
+        cin >> from >> to >> cost;
 
-        // 단방향 연결
-        // 멀티 소스 다익스트라 알고리즘을 적용하기 위해 간선을 거꾸로 저장
-        edges[dest].push_back(Edge(src, distance));
+        // 면접장에서 도시 방향으로 탐색하기 위해 간선을 거꾸로 저장
+        edges[to].push_back(Edge(from, cost));
     }
-
-    multiSources.assign(K, 0);
 
     // 면접장이 배치된 도시 번호 저장
     for (int i = 0; i < K; i++) {
-        cin >> multiSources[i];
+        cin >> interviewSites[i];
     }
 
-    minDistances.assign(N + 1, INF);
-
     // 도시별 가장 가까운 면접장까지의 최단 거리 계산
-    findMinDistances();
+    dijkstra(interviewSites);
 
-    int farthestNode = 1;
-    long long maxDistance = 0LL;
-    
-    for (int node = 1; node <= N; node++) {
-        if (maxDistance < minDistances[node]) {
-            farthestNode = node;
-            maxDistance = minDistances[node];
+    int farthestCity = 1; // 면접장까지 거리가 가장 먼 도시 번호
+    long long farthestDist = 0LL; // 해당 도시에서 면접장까지의 거리
+
+    for (int city = 1; city <= N; city++) {
+        if (farthestDist < dist[city]) {
+            farthestCity = city;
+            farthestDist = dist[city];
         }
     }
 
-    cout << farthestNode << "\n" << maxDistance;
-    
+    cout << farthestCity << "\n" << farthestDist;
+
     return 0;
 }
 
-void findMinDistances() {
-    priority_queue<Edge> PQ;
+void dijkstra(const vector<int>& sources) {
+    priority_queue<State> pq;
 
     // 면접장 초기 방문
-    for (int src : multiSources) {
-        PQ.push(Edge(src, 0LL));
-        minDistances[src] = 0;
+    for (int src : sources) {
+        dist[src] = 0LL;
+        pq.push(State(src, dist[src]));
     }
 
-    while (!PQ.empty()) {
-        auto [curr, currDistance] = PQ.top(); PQ.pop();
+    while (!pq.empty()) {
+        State curr = pq.top(); pq.pop();
 
-        if (minDistances[curr] < currDistance) {
+        // 이미 최단 거리가 확정된 경우 무시
+        if (dist[curr.node] < curr.dist) {
             continue;
         }
 
-        for (const auto& [next, nextDistance] : edges[curr]) {
-            if (minDistances[next] > currDistance + nextDistance) {
-                PQ.push(Edge(next, currDistance + nextDistance));
-                minDistances[next] = currDistance + nextDistance;
+        for (const Edge& edge : edges[curr.node]) {
+            if (dist[edge.dest] > curr.dist + edge.cost) {
+                dist[edge.dest] = curr.dist + edge.cost;
+                pq.push(State(edge.dest, dist[edge.dest]));
             }
         }
     }
