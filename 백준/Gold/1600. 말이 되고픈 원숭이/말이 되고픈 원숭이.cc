188b158b032eedcc5e2a0ef16e28f946bc58df40
Jisu Kim <jisukim.tech@gmail.com>
2026-02-27 20:13:53 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 76 ms, Memory: 2824 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -1,96 +1,111 @@
 #define Coord pair<int, int>
 #define X first
 #define Y second
-
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct Node {
     int x;
     int y;
-    int moveCount; // 이동 횟수
-    int useCount; // 말 능력 사용 횟수
+    int remainingSkillCount; // 사용 가능한 능력 횟수
+    int moveCount;
 
-    Node(int x, int y, int moveCount, int useCount) {
+    Node(int x, int y, int remainingSkillCount, int moveCount) {
         this->x = x;
         this->y = y;
+        this->remainingSkillCount = remainingSkillCount;
         this->moveCount = moveCount;
-        this->useCount = useCount;
     }
 };
 
-const int OBSTACLE = 1;
+const int OBSTACLE = 1; // 장애물
+
 const vector<Coord> horseMoves = {{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
 const vector<Coord> monkeyMoves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-int main() {
-    int K, W, H;
+int K, W, H;
 
+// 프로토타입
+bool isOutOfRange(int, int);
+
+int main() {
     cin >> K;
     cin >> W >> H;
 
     vector<vector<int>> grid(H, vector<int>(W));
 
-    // 격자 저장
+    // 격자판 정보 저장
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
             cin >> grid[i][j];
         }
     }
 
-    queue<Node> Q;
-    vector<vector<vector<bool>>> visited(K + 1, vector<vector<bool>>(H, vector<bool>(W, false))); // (K + 1) * H * W 크기 방문 여부 배열
-    Coord start = make_pair(0, 0); // 출발 지점
-    Coord end = make_pair(H - 1, W - 1); // 도착 지점
+    // 출발지, 도착지 정의
+    Coord source = make_pair(0, 0);
+    Coord destination = make_pair(H - 1, W - 1);
 
-    Q.push(Node(start.X, start.Y, 0, 0));
-    visited[0][start.X][start.Y] = true; // 능력을 0번 사용한 상태로 (start.X, start.Y) 방문
+    queue<Node> Q;
+    vector<vector<vector<bool>>> visited(K + 1, vector<vector<bool>>(H, vector<bool>(W, false)));
+
+    Q.push(Node(source.X, source.Y, K, 0));
+    visited[K][source.X][source.Y] = true;
 
     int answer = -1;
 
-    // 너비 우선 탐색 수행
+    // 너비 우선 탐색 기반 최소 이동 횟수 계산
     while (!Q.empty()) {
-        auto [x, y, moveCount, useCount] = Q.front(); Q.pop();
+        Node curr = Q.front(); Q.pop();
 
-        // 도착 지점에 도달한 경우 최소 이동 횟수 저장 후 탐색 종료
-        if (x == end.X && y == end.Y) {
-            answer = moveCount;
+        // 도착지에 다다른 경우 이동 횟수 반환 후 탐색 종료
+        if (curr.x == destination.X && curr.y == destination.Y) {
+            answer = curr.moveCount;
             break;
         }
 
-        // 말 능력을 사용하는 경우
-        if (useCount < K) {
+        // 능력을 사용해서 평지로 이동하는 경우
+        if (curr.remainingSkillCount >= 1) {
             for (const auto& [dx, dy] : horseMoves) {
-                int nx = x + dx;
-                int ny = y + dy;
+                int nx = curr.x + dx;
+                int ny = curr.y + dy;
 
-                if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
-                if (visited[useCount + 1][nx][ny]) continue;
-                if (grid[nx][ny] == OBSTACLE) continue;
+                if (isOutOfRange(nx, ny) || grid[nx][ny] == OBSTACLE) {
+                    continue;
+                }
 
-                Q.push(Node(nx, ny, moveCount + 1, useCount + 1));
-                visited[useCount + 1][nx][ny] = true; // 능력을 한 번 더 사용한 상태로 (nx, ny) 방문
+                if (visited[curr.remainingSkillCount - 1][nx][ny]) {
+                    continue;
+                }
+
+                Q.push(Node(nx, ny, curr.remainingSkillCount - 1, curr.moveCount + 1));
+                visited[curr.remainingSkillCount - 1][nx][ny] = true;        
             }
         }
 
-        // 말 능력을 사용하지 않는 경우
+        // 능력을 사용하지 않고 평지로 이동하는 경우
         for (const auto& [dx, dy] : monkeyMoves) {
-            int nx = x + dx;
-            int ny = y + dy;
+            int nx = curr.x + dx;
+            int ny = curr.y + dy;
 
-            if (nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
-            if (visited[useCount][nx][ny]) continue;
-            if (grid[nx][ny] == OBSTACLE) continue;
+            if (isOutOfRange(nx, ny) || grid[nx][ny] == OBSTACLE) {
+                continue;
+            }
 
-            Q.push(Node(nx, ny, moveCount + 1, useCount));
-            visited[useCount][nx][ny] = true; // 능력을 사용하지 않은 상태로 (nx, ny) 방문
+            if (visited[curr.remainingSkillCount][nx][ny]) {
+                continue;
+            }
+
+            Q.push(Node(nx, ny, curr.remainingSkillCount, curr.moveCount + 1));
+            visited[curr.remainingSkillCount][nx][ny] = true;   
         }
     }
 
     cout << answer;
     
     return 0;
+}
+
+bool isOutOfRange(int x, int y) {
+    return x < 0 || x >= H || y < 0 || y >= W;
 }
