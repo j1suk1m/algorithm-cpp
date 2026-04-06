6433898c91b7ea69a30cf6c80de15e592042299a
Jisu Kim <jisukim.tech@gmail.com>
2026-04-06 21:53:49 +0900

    [Gold IV] Title: 연구소, Time: 80 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,58 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
+#define Coord pair<int, int>
+#define X first
+#define Y second
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int answer;
 int N, M;
-int initialSafetyArea;
 
-vector<vector<int>> lab;
-vector<pair<int, int>> emptySpaces;
-vector<pair<int, int>> viruses;
+vector<Coord> initSpaces;
+vector<Coord> initViruses;
 
-const int BUILD_COUNT = 3;
+queue<Coord> Q;
 
-// 연구소 좌표 값
-const int EMPTY_SPACE = 0;
+const int NEW_WALL_COUNT = 3;
+
+const int SPACE = 0;
 const int WALL = 1;
 const int VIRUS = 2;
 
-// 상하좌우 이동
-const vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+const vector<Coord> dr = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 // 프로토타입
 bool isOutOfRange(int x, int y);
-int spreadViruses(vector<vector<int>> lab);
-void buildWalls(int buildCount, int index);
+int spread(const vector<vector<int>>& maps);
 
 int main() {
+    // 빠른 입출력 설정
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N >> M;
 
-    // 연구소 크기 재조정
-    lab.resize(N);
+    vector<vector<int>> maps(N, vector<int>(M));
 
-    for (int i = 0; i < N; i++) {
-        lab[i].resize(M);
-    }
+    int initWallCount = 0;
 
     // 연구소 지도 저장
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            cin >> lab[i][j];
-
-            if (lab[i][j] == EMPTY_SPACE) {
-                emptySpaces.push_back(make_pair(i, j));
-                initialSafetyArea++;
-            } else if (lab[i][j] == VIRUS) {
-                viruses.push_back(make_pair(i, j));
+            cin >> maps[i][j];
+            
+            if (maps[i][j] == SPACE) {
+                initSpaces.push_back(Coord(i, j));
+            } else if (maps[i][j] == VIRUS) {
+                initViruses.push_back(Coord(i, j));
+            } else if (maps[i][j] == WALL) {
+                initWallCount++;
             }
         }
     }
 
-    buildWalls(0, 0);
+    int answer = 0;
+    int initSpaceCount = (int)initSpaces.size();
+    int initVirusCount = (int)initViruses.size();
+    
+    for (int i = 0; i < initSpaceCount - 2; i++) {
+        Coord first = initSpaces[i];
+        
+        for (int j = i + 1; j < initSpaceCount - 1; j++) {
+            Coord second = initSpaces[j];
+            
+            for (int k = j + 1; k < initSpaceCount; k++) {
+                Coord third = initSpaces[k];
+                
+                // 벽 세우기
+                maps[first.X][first.Y] = WALL;
+                maps[second.X][second.Y] = WALL;
+                maps[third.X][third.Y] = WALL;
+
+                // 바이러스 확산 후 안전 영역의 크기 계산
+                int spreadCount = spread(maps);
+                int safeAreaSize = N * M - initWallCount - NEW_WALL_COUNT - initVirusCount - spreadCount;
+
+                // 안전 영역의 최대 크기 갱신
+                answer = max(answer, safeAreaSize);
+
+                // 벽 허물기
+                maps[first.X][first.Y] = SPACE;
+                maps[second.X][second.Y] = SPACE;
+                maps[third.X][third.Y] = SPACE;
+            }
+        }
+    }
 
     cout << answer;
     
@@ -63,47 +92,37 @@ bool isOutOfRange(int x, int y) {
     return x < 0 || x >= N || y < 0 || y >= M;
 }
 
-// 너비 우선 탐색으로 바이러스 확산
-int spreadViruses(vector<vector<int>> lab) {
-    queue<pair<int, int>> q;
-    int safetyArea = initialSafetyArea - BUILD_COUNT;
+int spread(const vector<vector<int>>& maps) {
+    int spreadCount = 0;
+    vector<vector<int>> temp = maps;
+    vector<vector<bool>> visited(N, vector<bool>(M, false));
 
-    // 초기 바이러스 좌표 큐에 저장
-    for (const auto& p : viruses) {
-        q.push(p);
+    for (const Coord& virus : initViruses) {
+        Q.push(virus);
+        visited[virus.X][virus.Y] = true;
     }
 
-    while (!q.empty()) {
-        auto [x, y] = q.front(); q.pop();
+    while (!Q.empty()) {
+        auto [x, y] = Q.front(); Q.pop();
 
-        for (const auto& [dx, dy] : directions) {
+        for (const auto& [dx, dy] : dr) {
             int nx = x + dx;
             int ny = y + dy;
 
-            if (isOutOfRange(nx, ny)) continue;
-            if (lab[nx][ny] != EMPTY_SPACE) continue;
+            if (isOutOfRange(nx, ny) || visited[nx][ny]) {
+                continue;
+            }
 
-            q.push(make_pair(nx, ny));
-            lab[nx][ny] = VIRUS;
-            safetyArea--;
+            if (temp[nx][ny] != SPACE) {
+                continue;
+            }
+
+            Q.push(Coord(nx, ny));
+            temp[nx][ny] = VIRUS;
+            visited[nx][ny] = true;
+            spreadCount++;
         }
     }
 
-    return safetyArea;
-}
-
-// 벽을 세우고 바이러스를 확산시킨 뒤 안전 영역의 최대 개수 갱신
-void buildWalls(int buildCount, int index) {
-    if (buildCount == BUILD_COUNT) {
-        int safetyArea = spreadViruses(lab);
-        answer = max(answer, safetyArea);
-        return;
-    }
-
-    for (int i = index; i < emptySpaces.size(); i++) {
-        auto [x, y] = emptySpaces[i];
-        lab[x][y] = WALL;
-        buildWalls(buildCount + 1, i + 1);
-        lab[x][y] = EMPTY_SPACE;
-    }
+    return spreadCount;
 }
